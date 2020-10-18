772745f26bc519fbbedbec36da4ba14993a5f313
Cinthia Barbosa da Silva <66570013+cinthiabs@users.noreply.github.com>
2020-10-18 17:10:51 -0300

    Add files via upload

---
+++ BANCO ORDEM DE SERVIÇO.sql
@@ -0,0 +1,32 @@
+create table cliente
+(codCliente int  identity primary key,
+Cidade varchar(30),
+Estado varchar(30),
+Endereco varchar(50),
+Telefone varchar(20),
+);
+
+create table atendente
+(codAten int identity primary key,
+Nome varchar(30),
+data datetime,
+);
+
+create table serviço
+(codserv int identity primary key,
+servico varchar(80), 
+descricao varchar(80), 
+observacao varchar(80), 
+
+);
+create table chamado(
+codchamado int identity primary key,
+statusChamado  varchar(80), 
+resolucao  varchar(80), 
+codcliente int,
+codaten int,
+codservico int,
+constraint FK_codcliente foreign key(codcliente) references cliente(codCliente),
+constraint FK_codaten foreign key(codaten) references atendente(codAten),
+constraint FK_codservico foreign key(codservico) references serviço(codserv),
+);
