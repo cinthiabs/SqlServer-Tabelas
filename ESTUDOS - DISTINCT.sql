9beaafaaffd8a806b49aadb555f2b763567dd811
Cinthia Barbosa da Silva <66570013+cinthiabs@users.noreply.github.com>
2020-12-27 10:15:41 -0300

    Estudos

---
+++ ESTUDOS - DISTINCT.sql
@@ -0,0 +1,14 @@
+-- usando o distinct = para exibir apenas os valores diferentes 
+
+select distinct cliente
+from pedidos
+
+select * from pedidos
+
+select distinct cond_pagamento
+from pedidos
+
+select distinct cidade
+from clientes
+
+select * from clientes  
